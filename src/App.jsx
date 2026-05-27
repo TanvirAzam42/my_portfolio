8ec9077678d887fae32d663c57993f17679faafa
Autimatic_ <111979644+TanvirAzam42@users.noreply.github.com>
2026-05-27 22:28:10 +0530

    some more bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
-import Experience from "./Components/Experience.jsx";
+import Experience from "./components/Experience.jsx";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
